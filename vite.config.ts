4f0b61450bf01ec1dad858e31eb4f4165c56e78f
Sreejith S V <sreejith.the.prince@gmail.com>
2026-06-05 17:27:11 +0530

    basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { basename } from "path";
 
 export default defineConfig({
+  base: "/sales/", 
   plugins: [
     react(),
     tailwindcss(),
